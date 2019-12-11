849483f6eae1c45cd6c9157bb8f4fb5d10df506a
Michael <maicol.asus@gmail.com>
2019-12-11 01:57:03 +0000

    [add] adding advanced task 15

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    newstr = str
     if (n >= 0):
-        newstr = str[:n] + str[n + 1:]
-        return str
+        str = str[:n] + str[n + 1:]
+    return str
