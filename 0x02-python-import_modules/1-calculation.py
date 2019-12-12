ecb5cb3c460d14e82048da4d32f2ecc249286067
TzStrikerYT <maicol.asus@gmail.com>
2019-12-12 06:48:43 +0000

    [add] adding files for task 0 and 1 whit all permissions

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+from calculation_1 import add, sub, mul, div
+
+def main():
+    a = 10
+    b = 5
+    sign = ["+", "-", "*", "/"]
+    opc = {0: add, 1: sub, 2: mul, 3: div}
+    for i in opc:
+        print("{:d} {:} {:d} = {:d}".format(a, sign[i], b, opc[i](a, b)))
+
+if __name__ == "__main__":
+    main()
