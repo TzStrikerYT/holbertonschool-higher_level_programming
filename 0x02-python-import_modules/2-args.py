290729af096300e3c887ee8f8203314200eb54e1
Michael <maicol.asus@gmail.com>
2019-12-12 17:53:55 +0000

    [fix] fix pep8 style for checkers

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,7 @@ import sys
 def main():
     leng = len(sys.argv)
     print("{:d} argument{}".format(leng - 1, "s." if leng == 1 else
-                                    (":" if leng == 2 else "s:")))
+                                   (":" if leng == 2 else "s:")))
     a = 1
     for i in sys.argv[1:]:
         print("{:d}: {}".format(a, i))
