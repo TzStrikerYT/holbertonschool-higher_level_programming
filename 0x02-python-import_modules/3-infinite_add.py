cfb4da0812e1d830e63376e9bbfd9983b4dd0236
TzStrikerYT <maicol.asus@gmail.com>
2019-12-12 07:06:05 +0000

    [add] adding file for task 2 and 3 in holberton

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+
+def main():
+    add = 0
+    for i argv[1:]:
+        add += int(i)
+    print("{:d".format(add))
+
+if __name__ == "__main__":
+    main()
