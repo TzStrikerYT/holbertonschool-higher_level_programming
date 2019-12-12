d6c60c9fc498b03006bb63432ba083b187d4a086
TzStrikerYT <maicol.asus@gmail.com>
2019-12-12 09:14:39 +0000

    [fix] fix errors and redefinition of function

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,13 +3,13 @@ import sys
 
 
 def main():
-    length = len(sys.argv)
-    print("{:d]: argument{:}".format(l - 1, "." if l == 1 else
-                                     (":" if l == 2 else "s:")))
-    argp = 1
+    leng = len(sys.argv)
+    print("{:d}: argument{}".format(leng - 1, "." if leng == 1 else
+                                    (":" if leng == 2 else "s:")))
+    a = 1
     for i in sys.argv[1:]:
-        print("{:d}: {}".format(argp, i))
-        argp =+ 1
+        print("{:d}: {}".format(a, i))
+        a = a + 1
 
 if __name__ == "__main__":
     main()
