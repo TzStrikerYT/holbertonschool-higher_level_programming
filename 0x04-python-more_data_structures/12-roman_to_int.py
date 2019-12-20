518fbcb0cbb35e352c4df5e33634abc84893ca5f
TzStrikerYT <maicol.asus@gmail.com>
2019-12-20 00:11:08 +0000

    adding mandatory task

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    res = 0
+    num = 0
+    r_nums = {'I': 1,
+              'V': 5,
+              'X': 10,
+              'L': 50,
+              'C': 100,
+              'D': 500,
+              'M': 1000}
+    for i in reversed(roman_string):
+        num = r_nums[i]
+        res += num if res < num * 5 else -num
+    return res
