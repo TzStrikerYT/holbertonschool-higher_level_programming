aeb9e3ed7efdf73edd46e631d934958cd207e715
TzStrikerYT <maicol.asus@gmail.com>
2020-01-09 22:47:21 -0500

    [fix] fix some posible errors in the program

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -32,14 +32,13 @@ class Square:
         """ sets position variable of square class instance """
         if (type(value[0]) != int or type(value[1]) != int or
             type(value) != tuple or len(value) != 2 or
-            value[0] < 0 or value[1] < 0):
+                value[0] < 0 or value[1] < 0):
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = value
 
     def area(self):
         """ area of square :v """
         return self.__size ** 2
-
 
     def my_print(self):
         """ prints a square of # """
