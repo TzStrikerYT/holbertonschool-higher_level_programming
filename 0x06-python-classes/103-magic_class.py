96ae0ceb612dc40d8bcf33f0862e374083163d2f
TzStrikerYT <maicol.asus@gmail.com>
2020-01-09 23:13:39 -0500

    [fix] syntaxis and condition

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -5,8 +5,8 @@ import math
 class MagicClass:
     def __init__(self, radius=0):
         self.__radius = 0
-        if type(radius) is not int is not float:
-            raise('radius must be a number')
+        if type(radius) is not int and type(radius) is not float:
+            raise TypeError('radius must be a number')
         self.__radius = radius
 
     def area(self):
