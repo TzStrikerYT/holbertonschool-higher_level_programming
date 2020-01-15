b3ca6cd2a70cac9ef1ec60d38bb77d8a19a098b0
TzStrikerYT <maicol.asus@gmail.com>
2020-01-15 01:42:25 -0500

    [fix] fix condition

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,21 +6,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """ init atributes of the object rectangle """
-
-        if type(height) != int:
-            raise TypeError("height must be an integer")
-        if height < 0:
-            raise ValueError("height must be >= 0")
-        else:
-            self.height = height
-
-        if type(height) != int:
-            raise TypeError("width must be an integer")
-        if height < 0:
-            raise ValueError("width must be >= 0")
-        else:
-            self.width = width
-
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
 
     @property
