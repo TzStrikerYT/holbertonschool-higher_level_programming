5defc4c66dea07799e634d51228c60ad84ae7749
TzStrikerYT <maicol.asus@gmail.com>
2020-01-21 00:26:50 -0500

    [fix] fix test with my peer angel, i poopit

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,6 +4,5 @@ def add_attribute(obj, name, value):
     if hasattr(obj, "__dict__") or \
        (hasattr(obj, "__slots__") and name in obj.__slots__):
         setattr(obj, name, value)
-        print(obj.__dict__)
     else:
         raise TypeError("can't add new attribute")
