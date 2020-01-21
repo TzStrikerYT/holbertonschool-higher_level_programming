e8be7acbb3a2b01434ef55ca8f43929a906482e8
TzStrikerYT <maicol.asus@gmail.com>
2020-01-21 18:55:36 -0500

    [fix] fix load for loads

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -5,6 +5,6 @@ import json
 def load_from_json_file(filename):
     """ crates json object from file """
     with open(filename, mode="r", encoding="utf-8") as f:
-        _dict = json.load(f.read())
+        _dict = json.loads(f.read())
         f.close()
     return _dict
