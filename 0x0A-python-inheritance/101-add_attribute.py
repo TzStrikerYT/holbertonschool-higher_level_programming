b07e75d2769b4f19d30ea1770a2facce779fc7a5
TzStrikerYT <maicol.asus@gmail.com>
2020-01-20 22:02:03 -0500

    [fix] fix order of values in the function

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-def add_attribute(obj, value, name):
+def add_attribute(obj, name, value):
     """ Method that validate if it can ad sets an attributte """
     if hasattr(obj, "__dict__") or \
        (hasattr(obj, "__slots__") and name in obj.__slots__):
-        setattr(obj,value, name)
+        setattr(obj, name, value)
     else:
         raise TypeError("can't add new attribute")
