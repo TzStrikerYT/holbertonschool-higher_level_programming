4dcffd746d456cfb8b4e03c0e046c8b7589a0769
TzStrikerYT <maicol.asus@gmail.com>
2020-01-21 18:49:41 -0500

    [add] task 8 for project

---
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+
+
+def load_from_json_file(filename):
+    """ crates json object from file """
+    with open(filename, mode="r", encoding="utf-8") as f:
+        _dict = json.load(f.read())
+        f.close()
+    return _dict
