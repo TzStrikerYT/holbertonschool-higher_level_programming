0e425a30c74338744ff6f2739f65679c630c25b7
Michael <maicol.asus@gmail.com>
2020-03-03 19:16:54 +0000

    [fix] fix couts and identation for checkers

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,2 +1,3 @@
 --CREATES A USER
-GRANT ALL PRIVILEGES ON *.* TO user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+       IDENTIFIED BY 'user_0d_1_pwd';
