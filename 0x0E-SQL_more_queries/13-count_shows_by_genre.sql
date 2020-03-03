7762e001c34e1ccdfe2581dd8bec8b1459bc007e
Michael <maicol.asus@gmail.com>
2020-03-03 20:44:41 +0000

    [add] add task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,3 +1,3 @@
 -- Num of shows
-SELECT tv_genres.name AS genre COUNT(*) AS number_of_shows FROM tv_show_genres
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows FROM tv_show_genres
 LEFT JOIN tv_genres ON tv_genres.id = genre_id GROUP BY genre_id ORDER BY COUNT(*) DESC;
