b26e826be349f817606291504651196dd7ac7acd
Michael <maicol.asus@gmail.com>
2020-03-03 21:26:13 +0000

    [add] add task 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,5 @@
+-- shows and genre
+SELECT tv_show.title, tv_genres.name FROM tv_shows
+LEFT JOIN tv_shows_genres ON tv_show_genres.show_id = tv_shows.id
+LEFT JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
+ORDER BY tv_shows.title, tv_genres.name;
