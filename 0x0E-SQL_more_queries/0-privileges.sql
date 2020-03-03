cb8782f02a4656862d5a7c97002fa4cb9d6771cb
Michael <maicol.asus@gmail.com>
2020-03-03 19:06:11 +0000

    [fix] quit cuts for checkers

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,5 +1,5 @@
 -- Display privileges
 -- User 1
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR user_0d_1@localhost;
 -- User 2
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+SHOW GRANTS FOR user_0d_2@localhost;
