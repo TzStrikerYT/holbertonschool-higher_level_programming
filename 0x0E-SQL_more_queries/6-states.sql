9eb1d0dbf7b888e3e56a4daf708cdcea7483e7f8
Michael <maicol.asus@gmail.com>
2020-03-03 20:04:59 +0000

    [fix] fix a auto_increment command

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
--- id unique
+-- create states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
-       id INT UNIQUE AUTOINCREMENT NOT NULL PRYMARY KEY,
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRYMARY KEY,
        name VARCHAR(256) NOT NULL
 );
