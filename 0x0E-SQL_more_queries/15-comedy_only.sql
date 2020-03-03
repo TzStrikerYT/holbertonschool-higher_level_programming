ed83a4f78d9e6ebcc61791135c6fdcdbd89160b8
Michael <maicol.asus@gmail.com>
2020-03-03 21:18:42 +0000

    [add] task 15

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,4 +1,6 @@
 -- only comedy cado :V
 SELECT show.title FROM tv_genres genre, tv_show_genres tv, tv_shows show 
-WHERE genre.id = tv.genre_id AND tv.show_id = show.id AND show.title = "Comedy"
+WHERE genre.id = tv.genre_id 
+      AND tv.show_id = show.id 
+      AND genre.name = "Comedy"
 ORDER BY show.title ASC;
