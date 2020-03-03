aafb1db4ab5561f219371669dbbd4e625c7e6e2a
TzStrikerYT <maicol.asus@gmail.com>
2020-03-03 03:21:59 +0000

    [fix] fix filters to list and add limit to list

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- top 3 cities temp july and aug
-SELECT city, AVG(value) AS avge_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
