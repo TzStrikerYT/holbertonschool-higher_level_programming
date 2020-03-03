25d91018e2068e24d71c62383217a2f950554f1e
Michael <maicol.asus@gmail.com>
2020-03-03 19:30:36 +0000

    [fix] fix comments

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,5 @@
 --CREATES A USER
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost'
        IDENTIFIED BY 'user_0d_1_pwd';
--- GRANT PRIVILEGES
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
--- FLUSH
 FLUSH PRIVILEGES;
