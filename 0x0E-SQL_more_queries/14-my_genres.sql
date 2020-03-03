79299de103e03bfde129856339db50c6687436c0
Michael <maicol.asus@gmail.com>
2020-03-03 21:08:08 +0000

    [fix] names for tables

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,5 +1,4 @@
 -- My genres :D
-SELECT g.name FROM tv_genres gen, tv_show_genres tv, tv_shows sws WHERE gen.id = tv.genre_id
-       	      	   	     	  		     	      	  	AND tv.show_id = sws.id
-									AND sws.title = "Dexter"
-ORDER BY gen.name ASC;
+SELECT g.name FROM tv_genres g, tv_show_genres t, tv_shows s 
+WHERE g.id = t.genre_id AND t.show_id = s.id AND s.title = "Dexter"
+ORDER BY g.name ASC;
