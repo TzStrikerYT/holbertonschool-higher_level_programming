c71a37b406d12fcc01ab7e709bcebf8aa02b9416
Michael <maicol.asus@gmail.com>
2020-03-28 19:16:58 +0000

    [fix] fix pep8 styles

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,11 +7,12 @@ from sys import argv
 if __name__ == "__main__":
     """ Connection to DB """
     connection = MySQLdb.connect(host="localhost",
-                                port=3306,
-                                charset="utf8",
-                                user=argv[1],
-                                passwd=argv[2],
-                                db=argv[3])
+                                 port=3306,
+                                 charset="utf8",
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+
     """ Cursor that exec SQL query """
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
