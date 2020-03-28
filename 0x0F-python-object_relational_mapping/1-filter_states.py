f68bff4937690622863eefe0d9f4c96ea3550079
Michael <maicol.asus@gmail.com>
2020-03-28 19:19:36 +0000

    [fix] fix a space n the query and pep8 style

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,14 +7,16 @@ from sys import argv
 if __name__ == "__main__":
     """ Connection to DB """
     connection = MySQLdb.connect(host="localhost",
-                                port=3306,
-                                charset="utf8",
-                                user=argv[1],
-                                passwd=argv[2],
-                                db=argv[3])
+                                 port=3306,
+                                 charset="utf8",
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+
     """ Cursor that exec SQL query """
     cursor = connection.cursor()
-    cursor.execute("SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC")
+    cursor.execute("SELECT * FROM states WHERE states.name "
+                   "LIKE BINARY 'N%' ORDER BY states.id ASC")
 
     """ shows rows of rhe query """
     res_query = cursor.fetchall()
