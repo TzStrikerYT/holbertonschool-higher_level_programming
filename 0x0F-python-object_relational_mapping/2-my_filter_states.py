0e93bcc1f4363a7c42a43651e1876bb2173bb5c3
Michael <maicol.asus@gmail.com>
2020-03-28 08:33:04 +0000

    [add] add task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""List all data into DB"""
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    """ Connection to DB """
+    connection = MySQLdb.connect(host="localhost",
+                                 port=3306,
+                                 charset="utf8",
+                                 user=argv[1],
+                                 passwd=argv[2],
+                                 db=argv[3])
+    """ Cursor that exec SQL query """
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM states WHERE states.name LIKE BINARY"
+                   "'{}'ORDER BY states.id ASC".format(argv[4]))
+
+    """ shows rows of rhe query """
+    res_query = cursor.fetchall()
+
+    for i in res_query:
+        print(i)
+
+    """ Close connection and close cursor """
+    cursor.close()
+    connection.close()
