4962791e29803dd0c38053fc685134feeed0d0af
Michael <maicol.asus@gmail.com>
2020-03-30 16:26:09 +0000

    [add] add advanced

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -20,6 +20,6 @@ class State(Base):
     name = Column(String(128),
                   nullable=False)
     cities = relationship("City",
-                            cascade="all, delete-orphan",
-                            backref=backref("state", cascade="all"),
-                            single_parent=True)
+                          cascade="all, delete-orphan",
+                          backref=backref("state", cascade="all"),
+                          single_parent=True)
