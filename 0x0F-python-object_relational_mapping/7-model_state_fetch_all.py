e0f20fda3811fdd6f357b70fa336ef9099354ce1
Michael <maicol.asus@gmail.com>
2020-03-30 02:17:29 +0000

    [fix] fix identation

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -12,10 +12,10 @@ if __name__ == "__main__":
                                argv[1], argv[2], argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-Session = sessionmaker(bind=engine)
-session = Session()
+    Session = sessionmaker(bind=engine)
+    session = Session()
 
-""" Select * from state order by state.id ASC """
-for state in session.query(State).order_by(State.id).all():
-    print("{}: {}".format(state.id, state.name))
-session.close()
+    """ Select * from state order by state.id ASC """
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
+    session.close()
