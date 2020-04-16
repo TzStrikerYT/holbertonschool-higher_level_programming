15b015a69bbb0dfbe35a9051ee869501ece3f6dd
Michael <maicol.asus@gmail.com>
2020-04-16 16:40:51 +0000

    [add] add task 6

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Displays email request """
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    email = {"email": argv[2]}
+    request = requests.post(argv[1], data = email)
+    print(request.text)
