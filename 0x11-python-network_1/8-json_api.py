c94d3cae4e9256989ce72f710b45b5d40f6a64b1
Michael <maicol.asus@gmail.com>
2020-04-16 17:35:58 +0000

    [add] add task 8

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+
+    js = {"q": argv[1]} if len(argv[1]) == 1 else {"q": ""}
+
+    try:
+        request = requests.post("http://0.0.0.0:5000/search_user", data=js)
+        json = request.json()
+
+        if json:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        else:
+            print("No result")
+
+    except ValueError:
+        print("Not a valid JSON")
