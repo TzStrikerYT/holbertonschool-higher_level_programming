8196a293a3d9bb9f3a9f22a7749bc35e8e1af0e4
Michael <maicol.asus@gmail.com>
2020-05-12 18:27:15 +0000

    [add] add task 0)

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,2 +1,11 @@
 #!/usr/bin/node
-fs.writeFileSync(process.argv[3], fString);
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf-8', (error, data) => {
+  if (error) {
+    console.log(error);
+  }
+
+  if (data) {
+    console.log(data);
+  }
+});
