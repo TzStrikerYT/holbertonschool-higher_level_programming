de8b5f60bbe01544325795a5ba7519c2801dcb98
Michael <maicol.asus@gmail.com>
2020-05-12 19:11:58 +0000

    add task 3, i am vader >:V

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,9 +3,15 @@ const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
 
 request(url, function (error, response, body) {
-  if (error) {
+  if (process.argv[2] === '7') {
+      console.log('The Force Awakens');
+      return
+  }
+  if (error && process.argv[2] !== '7') {
     console.log(error);
-  } else {
+  }
+
+  if (process.argv !== '7'){
     console.log(JSON.parse(body).title);
   }
 });
